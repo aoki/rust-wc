2f73a0dd65402d8cef34ea8b7947c595f9c36cb6
aoki <ringohub@gmail.com>
2021-10-21 10:54:54 +0900

    Implements word count

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{
+    collections::HashMap,
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).expect("1 argument FILENAME required");
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(&file);
+
+    let freqs = count(reader);
+    println!("{:?}", freqs);
+}
+
+pub fn count(input: impl BufRead) -> HashMap<String, usize> {
+    let re = Regex::new(r"\w+").unwrap();
+    let mut freqs = HashMap::new();
+
+    for line in input.lines() {
+        let line = line.unwrap();
+        for m in re.find_iter(&line) {
+            let word = m.as_str().to_string();
+            *freqs.entry(word).or_insert(0) += 1;
+        }
+    }
+    freqs
 }
