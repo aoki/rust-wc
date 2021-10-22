b45dd2a61b78e88cdd4f96f5192b270f2394bfe6
aoki <ringohub@gmail.com>
2021-10-22 11:03:39 +0900

    Add test module and test file

--- src/lib.rs
+++ src/lib.rs
@@ -58,80 +58,79 @@ pub fn count(input: impl BufRead, option: CountOption) -> HashMap<String, usize>
     freqs
 }
 
-#[test]
-fn word_count_works() {
-    use std::io::Cursor;
-    let mut exp = HashMap::new();
-    exp.insert("aa".to_string(), 1);
-    exp.insert("bb".to_string(), 2);
-    exp.insert("cc".to_string(), 1);
-
-    assert_eq!(count(Cursor::new("aa bb cc bb"), CountOption::Word), exp);
-}
-
-#[test]
-fn word_count_works2() {
+#[cfg(test)]
+mod test {
+    use super::*;
     use std::io::Cursor;
 
-    let mut exp = HashMap::new();
-    exp.insert("aa".to_string(), 1);
-    exp.insert("cc".to_string(), 1);
-    exp.insert("dd".to_string(), 1);
+    #[test]
+    fn word_count_works() {
+        let mut exp = HashMap::new();
+        exp.insert("aa".to_string(), 1);
+        exp.insert("bb".to_string(), 2);
+        exp.insert("cc".to_string(), 1);
 
-    assert_eq!(count(Cursor::new("aa cc dd"), CountOption::Word), exp);
-}
+        assert_eq!(count(Cursor::new("aa bb cc bb"), CountOption::Word), exp);
+    }
 
-#[test]
-fn assert_test() {
-    let a = 3;
-    let b = 27;
-    assert!(a + b == 30, "a = {}, b = {}", a, b);
-}
+    #[test]
+    fn word_count_works2() {
+        let mut exp = HashMap::new();
+        exp.insert("aa".to_string(), 1);
+        exp.insert("cc".to_string(), 1);
+        exp.insert("dd".to_string(), 1);
 
-#[test]
-fn result_test() -> std::io::Result<()> {
-    use std::fs::{read_to_string, remove_file, write};
-    write("test.txt", "message")?;
-    let message = read_to_string("test.txt")?;
-    remove_file("test.txt")?;
-    assert_eq!(message, "message");
-    Ok(())
-}
+        assert_eq!(count(Cursor::new("aa cc dd"), CountOption::Word), exp);
+    }
 
-#[test]
-#[should_panic]
-fn word_count_do_not_contain_unknown_words() {
-    use std::io::Cursor;
+    #[test]
+    fn assert_test() {
+        let a = 3;
+        let b = 27;
+        assert!(a + b == 30, "a = {}, b = {}", a, b);
+    }
 
-    count(
-        Cursor::new([b'a', 0xf0, 0x90, 0x80, 0xe3, 0x81, 0x82]),
-        CountOption::Word,
-    );
-}
+    #[test]
+    fn result_test() -> std::io::Result<()> {
+        use std::fs::{read_to_string, remove_file, write};
+        write("test.txt", "message")?;
+        let message = read_to_string("test.txt")?;
+        remove_file("test.txt")?;
+        assert_eq!(message, "message");
+        Ok(())
+    }
 
-#[test]
-#[ignore]
-// `cargo test -- --ignored` を渡すと実行することができる
-fn large_test() {
-    std::thread::sleep(std::time::Duration::from_secs(5));
-}
+    #[test]
+    #[should_panic]
+    fn word_count_do_not_contain_unknown_words() {
+        count(
+            Cursor::new([b'a', 0xf0, 0x90, 0x80, 0xe3, 0x81, 0x82]),
+            CountOption::Word,
+        );
+    }
 
-#[allow(unused_macros)]
-// Mapの複数のKey-Valueペアに対してassertをするマクロを定義しておく
-macro_rules! assert_map {
+    #[test]
+    #[ignore]
+    // `cargo test -- --ignored` を渡すと実行することができる
+    fn large_test() {
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    }
+
+    // #[allow(unused_macros)]
+    // Mapの複数のKey-Valueペアに対してassertをするマクロを定義しておく
+    macro_rules! assert_map {
     ($expr: expr, {$($key: expr => $value: expr),*}) => {
         $(assert_eq!($expr[$key], $value));*
     };
 }
 
-#[test]
-fn word_count_works3() {
-    use std::io::Cursor;
-
-    let freqs = count(Cursor::new("aa cc dd"), CountOption::Word);
-    assert_eq!(freqs.len(), 3);
-    assert_map!(freqs, {"aa" => 1, "cc" => 1, "dd" => 1});
-    // assert_eq!(freqs["aa"], 1);
-    // assert_eq!(freqs["cc"], 1);
-    // assert_eq!(freqs["dd"], 1);
+    #[test]
+    fn word_count_works3() {
+        let freqs = count(Cursor::new("aa cc dd"), CountOption::Word);
+        assert_eq!(freqs.len(), 3);
+        assert_map!(freqs, {"aa" => 1, "cc" => 1, "dd" => 1});
+        // assert_eq!(freqs["aa"], 1);
+        // assert_eq!(freqs["cc"], 1);
+        // assert_eq!(freqs["dd"], 1);
+    }
 }
